d5fed88730a8be38c3ac0ef2404da27e20de4bb9
Tenjizen <92018813+Tenjizen@users.noreply.github.com>
2022-04-04 09:17:46 +0200

    Revert "Revert "Intégration Arbre NFT""
    This reverts commit bc957f134c86eb75c0b8a662db1ce3091a9afc6c.

---
+++ Assets/_Scripts/_Menu/DataCenter/MenuPrincipale.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipale : MonoBehaviour
+{
+    public void OnClickBtn()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
